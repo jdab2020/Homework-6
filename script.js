24b7f93479ea32a167dfc3e9c5844f4576a8b1af
jdab2020 <jdabalos90@gmail.com>
2020-02-04 22:59:10 -0800

    added icons

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    let currentdate = new Date ();
+    let day = currentdate.getDate();
+    let month = currentdate.getMonth();
+    let year = currentdate.getFullYear();
+    // set a variable | check if local storage is empty or not | if it isn't then render the cities under history sidebar as clickable
     let cities = [];
     renderLocalStorage();
-
     function renderLocalStorage () {
         let test = localStorage.getItem("savedCity");
         if (!test) {
@@ -13,7 +17,7 @@ $(document).ready(function () {
             renderSideBar(city);
         });
     }
-    // function to display each city
+    // function to display saved searched city on the sidebar as clickable to be displayed with current information
     function renderSideBar(city) {
         let cityli = $("<li>");
         $(cityli).addClass("nav-item");
@@ -29,11 +33,7 @@ $(document).ready(function () {
     //     renderCity(city)
     // })
 
-    
-
-
-
-    // render city
+    // main function to get information on current weather, uv, and 5-day forecast
     function renderCity(city) {
         var apiKey = "&appid=712bd548fef61a9da60836878da0b06d"
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
@@ -51,12 +51,14 @@ $(document).ready(function () {
             let temperatureF = ((Number(temperatureK) - 273.15) * 1.8) + 32;
             let humidity = response.main.humidity;
             let windSpeed = response.wind.speed;
+            let iconCode = response.weather[0].icon;
+            // console.log(iconCode);
             renderUV(apiKey,latitude,longitude);
-            renderWeather(cityName,temperatureF,humidity,windSpeed);
+            renderWeather(cityName,temperatureF,humidity,windSpeed,iconCode);
             renderFiveDay(apiKey,city);
         })
     }
-    // grab info about uv
+    // grab info about uv and display
     function renderUV(apiKey,latitude,longitude) {
         let queryUV = "http://api.openweathermap.org/data/2.5/uvi?" + apiKey + "&lat=" + latitude + "&lon=" + longitude;
 
@@ -70,7 +72,7 @@ $(document).ready(function () {
             $("#uv-index").text("UVI: " + UV);
         })
     }
-    // grab info about 5day forecast
+    // grab info about 5day forecast and display
     function renderFiveDay (apiKey,city) {
         let queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
         $.ajax({
@@ -78,16 +80,21 @@ $(document).ready(function () {
             method: "GET"
         }).then(function(response) {
             $("#forecast").empty();
-            // console.log(response);
+            console.log(response);
             for (let i = 7; i <= response.list.length ; i += 8) {
                 temperatureK = response.list[i].main.temp;
                 temperatureF = ((Number(temperatureK) - 273.15) * 1.8) + 32;
                 humidity = response.list[i].main.humidity;
                 let dateTime = response.list[i].dt_txt;
                 let date = dateTime.split(" ");
+                let iconI = $("<img>");
+                let iconCode = response.list[i].weather[0].icon;
+                let iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png"
+                $(iconI).attr("src", iconURL);
                 let cityInfo = $("<div>");
                 $(cityInfo).addClass("ml-2 pl-2 col-sm-12 col-md-4 col-xl-2 pb-2 mb-2 border");
                 $(cityInfo).append("<h4> " + date[0] + "</h4><br>");
+                $(cityInfo).append(iconI);
                 $(cityInfo).append("<h6> Temperature: " + temperatureF.toFixed(2) + "&#8457</h6><br>");
                 $(cityInfo).append("<h6> Humidity: " + humidity + "%</h6><br>");
                 $("#forecast").append(cityInfo);
@@ -95,11 +102,15 @@ $(document).ready(function () {
             $("#forecastHeader").html("5-Day Forecast")
         })
     }
-    // renders current weather
-    function renderWeather(cityName,temperatureF,humidity,windSpeed) {
+    // displays current weather
+    function renderWeather(cityName,temperatureF,humidity,windSpeed,iconCode) {
         let cityInfo = $("<div>");
+        let iconI = $("<img>");
+        let iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png"
+        $(iconI).attr("src", iconURL);
         $(cityInfo).addClass("ml-2 pl-2");
-        $(cityInfo).append("<h2>" + cityName + "</h2><br>");
+        $(cityInfo).append("<h2>" + cityName + " (" + year + "/" + month + "/" + day + ")" + "</h2>");
+        $(cityInfo).append(iconI);
         $(cityInfo).append("<h4> Temperature (F): " + temperatureF.toFixed(2) + "&#8457</h4><br>");
         $(cityInfo).append("<h4> Humidity: " + humidity + "%</h4><br>");
         $(cityInfo).append("<h4> Wind Speed: " + windSpeed + " MPH</h4><br>");
@@ -109,7 +120,7 @@ $(document).ready(function () {
         $("#currentWeather").html(cityInfo);
     }
 
-    // input event 
+    // input event for desired city | adds a click event per new city | so it's clickable right away
     $("button").on("click", function (event) {
         event.preventDefault();
         let newCity = $("#search-input").val().trim();
@@ -121,9 +132,9 @@ $(document).ready(function () {
         renderSideBar(newCity);
         $(".nav-item").on("click", navClickHandler);
     })
-    
+    // click event for previously saved searches
     $(".nav-item").on("click", navClickHandler);
-
+    // function that will run after each click event
     function navClickHandler () {
         let city = $(this).text();
         renderCity(city);
